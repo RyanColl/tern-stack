0cd0088a07d0603f06c06191be09b72a28349e9e
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-01 15:08:50 -0700

    added express for ssr

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import useSWR, { mutate } from 'swr'
 import toast from 'react-hot-toast'
 import redis from '../lib/redis'
 
+
+
 const fetcher = (url) => fetch(url).then((res) => res.json())
  
 function LoadingSpinner({ invert }) {
