4bf2687b7c8ffe3198828217f194ec5378d959cc
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-02 08:47:32 -0700

    logos added

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,8 @@ const IndexPage = ({ logo }) => {
   return(
     <div className="App">
     <header className="App-header">
-      <img src={'./logo.svg'} className="App-logo" alt="logo" />
-      <p>
-        This app is completely set up with TailWind, Redux, Redis, Express and Nextjs.
-      </p>
-      <p>
-        It is an all in one server and react rendering app for optimized seo alongside large app productions.
-        </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <img src={'./logo-p1.png'} className="App-logo" alt="logo" />
+      <span>RCOLL DEV</span>
     </header>
   </div>
 )};
